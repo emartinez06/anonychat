3b64c561a5444a8c7c0b03ddbc49d2d8af427877
emartinez06 <mail@devmartinez.net>
2019-02-02 12:04:21 -0400

    Update to template: Update on index route file to update text on home page. Removal of livereload script.

--- routes/index.js
+++ routes/index.js
@@ -1,11 +1,11 @@
 // Import Express and Router
-var express = require('express');
-var router = express.Router();
+let express = require('express');
+let router = express.Router();
 
 // Get
 router.get('/', function(req, res) {
     res.render('index', {
-        title: 'AnonyChat - Chat with no traces',
+        title: 'AnonyChat - Chat without fear',
         lead: 'Start with your user name and let\'s chat'
     });
 });
